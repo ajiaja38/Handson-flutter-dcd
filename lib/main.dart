a5b3c1bf926cf8e314be911ea460bfa9af77d172
ajiaja38 <ajicooljazz38@gmail.com>
2025-04-27 21:12:23 +0700

    feat: update handson

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,8 @@ class BiggerText extends StatefulWidget {
 
 class _BiggerTextState extends State<BiggerText> {
   double _textSize = 16.0;
+  String? language;
+  String _name = '';
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +60,7 @@ class _BiggerTextState extends State<BiggerText> {
         ),
         Container(
           padding: EdgeInsets.all(10),
-          margin: EdgeInsets.only(top: 13),
+          margin: EdgeInsets.symmetric(vertical: 10),
           decoration: BoxDecoration(
             color: Colors.blue,
             borderRadius: BorderRadius.only(
@@ -68,19 +70,123 @@ class _BiggerTextState extends State<BiggerText> {
             boxShadow: const [
               BoxShadow(
                 color: Colors.black,
-                offset: Offset(3, 6),
-                blurRadius: 10,
+                offset: Offset(0.5, 4),
+                blurRadius: 8,
               ),
             ],
           ),
           child: Text(
             "Halo Aji",
-            style: TextStyle(color: Colors.white, fontSize: 30),
+            style: TextStyle(color: Colors.white, fontSize: 15),
+          ),
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+          onPressed: () {},
+          child: Text.rich(
+            TextSpan(
+              text: 'Your language is ',
+              children: [
+                TextSpan(
+                  text: language ?? 'Java',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[100],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<String>(
+              items: const <DropdownMenuItem<String>>[
+                DropdownMenuItem<String>(value: 'Dart', child: Text('Dart')),
+                DropdownMenuItem<String>(
+                  value: 'Kotlin',
+                  child: Text('Kotlin'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'React Native',
+                  child: Text('TS'),
+                ),
+              ],
+              hint: Text(
+                'Select Language',
+                style: TextStyle(color: Colors.white),
+              ),
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                });
+              },
+            ),
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(30),
-          child: const Text("Ini Padding"),
+          padding: EdgeInsets.only(top: 12, left: 10, right: 10),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Write your name...",
+              labelText: 'Yourname',
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue, width: 2),
+              ),
+            ),
+            onChanged: (String value) {
+              setState(() {
+                _name = value;
+              });
+            },
+          ),
+        ),
+        const SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text("Halo! $_name"),
+                  title: Text("Dialog"),
+                  backgroundColor: Colors.white,
+                  titleTextStyle: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5)),
+                  ),
+                );
+              },
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+          child: const Text("Submit"),
+        ),
+        const SizedBox(height: 10),
+        Center(
+          child: Image.network(
+            'https://picsum.photos/200/300',
+            width: 200,
+            height: 200,
+          ),
         ),
       ],
     );
@@ -111,7 +217,10 @@ class FirstScreen extends StatelessWidget {
           icon: const Icon(Icons.menu, color: Colors.white),
         ),
       ),
-      body: const Center(child: BiggerText(text: "Hello World")),
+      body: const Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[BiggerText(text: "Hello World")],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.blue,
